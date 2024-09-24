fe36c635d53477a960af4323e729353bac189356
Bagas Dwi Sulistyo <bag12dwi.s@gmail.com>
2024-09-25 05:58:39 +0700

    edited migration

--- database/migrations/2024_09_24_221007_create_comment_discussions_table.php
+++ database/migrations/2024_09_24_221007_create_comment_discussions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comment_discussions', function (Blueprint $table) {
             $table->id();
             $table->text('comment');
-            $table->foreignId('diskusi_id')->constrained()->onDelete('cascade');
+            $table->foreignId('discussion_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('parent_id')->nullable()->constrained('comment_discussions')->onDelete('cascade');
             $table->timestamps();
